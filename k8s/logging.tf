2aefdc5873ee07c72125f68cdf23cef4332f8129
Denis Slavchik <d.slavchik@gmail.com>
2025-02-16 14:22:03 +0300

    Update logging group name in logging.tf for compliance with naming conventions

--- k8s/logging.tf
+++ k8s/logging.tf
@@ -1,5 +1,6 @@
 resource "yandex_logging_group" "k8s_logs" {
-  name        = "k8s_logs"
+  # Название может содержать только буквы, цифры и "-"
+  name        = "k8s-logs"
   description = "Log group for Kubernetes master logs"
   folder_id   = var.folder_id
 }
